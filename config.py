ff4888e330efb1f639c88e40a73ba300a1e6a4d6
Andrei Burvel <burvelandrei@gmail.com>
2025-03-29 13:05:07 +0300

    добавление конфига и либы httpx для запросов к yandex

---
+++ config.py
@@ -0,0 +1,32 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+
+class Settings(BaseSettings):
+    SERVER_HOST: str
+    SERVER_PORT: int
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+
+    YANDEX_CLIENT_ID: str
+    YANDEX_CLIENT_SECRET: str
+    REDIRECT_URI: str = (
+        f"http://{SERVER_HOST}:{SERVER_PORT}/auth/yandex/callback"
+    )
+
+    UPLOAD_DIR: str = "uploads"
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config = ConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
