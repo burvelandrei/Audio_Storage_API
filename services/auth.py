85781ad0e2f85f0d68b98742f8a68aae785d7752
Andrei Burvel <burvelandrei@gmail.com>
2025-03-29 14:08:45 +0300

    добавлен services для аутентификации

---
+++ services/auth.py
@@ -0,0 +1,83 @@
+import jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.connect import get_session
+from db.operations import UserDO
+from schemas.user import UserOut
+from config import settings
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def create_access_token(data: dict, secret_key: str = settings.SECRET_KEY):
+    """Функция создания access токена"""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        secret_key,
+        algorithm=settings.ALGORITHM,
+    )
+    return encoded_jwt
+
+
+def create_refresh_token(data: dict, secret_key: str = settings.SECRET_KEY):
+    """Функция создания refresh токена"""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        secret_key,
+        algorithm=settings.ALGORITHM,
+    )
+    return encoded_jwt
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(get_session),
+):
+    """Функция возврата юзера по access токену"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        user_id = payload.get("sub")
+        if not user_id:
+            raise credentials_exception
+        user = await UserDO.get_by_id(id=user_id, session=session)
+        if not user:
+            raise credentials_exception
+        return user
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired",
+        )
+    except jwt.InvalidTokenError:
+        raise credentials_exception
+
+
+async def check_admin(
+    user: UserOut = Depends(get_current_user),
+):
+    """Функция проверки на админа"""
+    if not user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have access to this resource",
+        )
+    return True
