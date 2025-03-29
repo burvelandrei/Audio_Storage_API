593dd401a87ba55871691eaaa6e9951eff046d15
Andrei Burvel <burvelandrei@gmail.com>
2025-03-29 15:35:00 +0300

    добавлены функции для создания директорий и сохранения файла, также добавлен lifespan в main для создания директории загрузок при запуске

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from routers import auth, files, users
 from utils.middlewares import (
     http_exception_handler,
     validation_exception_handler,
     global_exception_handler,
     LogRequestsMiddleware,
 )
+from utils.storage_utils import lifespan
 
 
-app = FastAPI(title="Audio Storage API")
+app = FastAPI(title="Audio Storage API", lifespan=lifespan)
+
 # Подключение миддлвари и обработчиков ошибок для логов
 app.add_middleware(LogRequestsMiddleware)
 app.add_exception_handler(StarletteHTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(Exception, global_exception_handler)
 # Подключение рутов
+app.include_router(auth.router)
+app.include_router(files.router)
+app.include_router(users.router)
 
 
 if __name__ == "__main__":
---
+++ utils/storage_utils.py
@@ -0,0 +1,62 @@
+import os
+import re
+import shutil
+import logging
+import logging.config
+from fastapi import FastAPI, UploadFile, HTTPException
+from contextlib import asynccontextmanager
+from utils.logger import logging_config
+from config import settings
+
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("storage_utils")
+
+
+def create_dir(dir_name: str):
+    """Создает директорию, если её нет"""
+    try:
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+            logger.info(f"Created directory")
+    except Exception as e:
+        logger.error(f"Failed to create directory: {e}")
+        raise
+
+
+def sanitize_filename(filename: str) -> str:
+    """Очищает имя файла от небезопасных символов"""
+    return re.sub(r"[^\w\-_.]", "_", filename)
+
+
+async def save_file(
+    file: UploadFile,
+    user_id: int,
+    custom_filename: str | None,
+) -> dict:
+    """Сохранение файла"""
+    user_upload_dir = os.path.join(settings.UPLOAD_DIR, str(user_id))
+    create_dir(user_upload_dir)
+    original_filename = file.filename
+    file_ext = os.path.splitext(original_filename)[1].lower()
+    final_filename = (
+        f"{sanitize_filename(custom_filename)}{file_ext}"
+        if custom_filename
+        else original_filename
+    )
+    filepath = os.path.join(user_upload_dir, final_filename)
+    try:
+        with open(filepath, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    except Exception as e:
+        raise HTTPException(500, detail=f"File save error")
+    return {
+        "filename": final_filename,
+        "filepath": filepath,
+    }
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    create_dir(dir_name=settings.UPLOAD_DIR)
+    yield
